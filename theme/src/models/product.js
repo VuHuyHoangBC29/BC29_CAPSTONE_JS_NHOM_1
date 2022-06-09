6ed48d54ddef84f739d097d3e624fcf6ae875cb3
longdaikkk <holongdai@gmail.com>
2022-06-09 22:39:25 +0700

    update theme

---
+++ theme/src/models/product.js
@@ -0,0 +1,12 @@
+class Product{
+    constructor(id, hinh, ten, soLuong, gia){
+        this.id = id;
+        this.hinhAnh = hinh;
+        this.ten = ten;
+        this.soLuong = soLuong;
+        this.gia = gia;
+    }
+    Xoa(){
+
+    }
+}
