f23439dfe412464a1598966e63d04ca033c511a4
longdaikkk <holongdai@gmail.com>
2022-06-11 23:11:56 +0700

    update cau 6 7

--- theme/src/controllers/main.js
+++ theme/src/controllers/main.js
@@ -1,8 +1,7 @@
 let services = new Services();
 let listProduct = new ListProduct();
-let dataApi = [];
-
-let arrCart = [];
+let productList = [];
+let cart = [];
 const getEle = (id) => {
     return document.getElementById(id);
 }
@@ -19,7 +18,7 @@ const getListProduct = () => {
     const promise = services.getProductApi();
     promise
         .then(function (result) {
-            dataApi = result.data;
+            productList = result.data;
             renderProduct(result.data);
         })
 
@@ -45,7 +44,7 @@ const renderProduct = (data) => {
                 <div class="icon d-flex justify-content-between p-3">${icon}</i>
                     <span>In Stock</span>
                 </div>
-                <img class="card-img-top mx-auto mt-4 mb-5"
+                <img class="card-img-top mx-auto"
                     src="${item.img}"
                     alt="Card image" style="width:70%">
                 <div class="card-body">
@@ -63,8 +62,8 @@ const renderProduct = (data) => {
                             </ul>
                         </div>
                         <div class="product_price d-flex justify-content-between align-items-center">
-                            <span>${item.price} VNĐ</span>
-                            <button class="btn" onclick="ThemSP(${item.id})">Add <i
+                            <span>$ ${item.price} VNĐ</span>
+                            <button class="btn text-light" onclick="ThemSP('${item.id}')">Add <i
                                     class="fa-solid fa-chevron-right"></i></button>
                         </div>
                     </div>
@@ -79,31 +78,57 @@ const renderProduct = (data) => {
 getEle('selectPhone').onchange = function () {
     const filter = getEle('selectPhone').value;
     let dataApiFilter = [];
-    dataApi.forEach((item, i) => {
+    productList.forEach((item, i) => {
         if (filter === item.type.toLowerCase()) {
             dataApiFilter.push(item);
         }
     })
     if (filter === "") {
-        renderProduct(dataApi);
+        renderProduct(ProductList);
     } else {
         renderProduct(dataApiFilter);
     }
 }
 
-const ThemSP = (id) =>{
-    dataApi.forEach((item) => {
-        if(item.id == id){
-            arrCart.push(item);
+
+
+const ThemSP = (id) => {
+    // dataApi.forEach((item) => {
+    //     if(item.id == id){
+    //         arrCart.push(item);
+    //     }
+    // })
+    // const product = new Product(item.id);
+    var index = "";
+    productList.forEach((item, i) => {
+        if (item.id == id) {
+            index = i;
+        }
+    });
+
+    var flag = -1;
+    cart.forEach((item, i)=>{
+        if(item.id === id){
+            flag = i;
+            item.soLuong++;
         }
     })
-    getEle('counter').innerHTML = arrCart.length;
-    if(arrCart.length > 0){
+    if(flag === -1){
+        let product = new Product(productList[index].id, productList[index].price, productList[index].name, productList[index].img);
+        cart.push(product);
+    }
+
+    var total = 0;
+    cart.forEach((item, i)=>{
+        total += item.soLuong
+    })
+    getEle('counter').innerHTML = total;
+    if (total > 0) {
         getEle('tbCart').style.display = "none";
-    }else{
+    } else {
         getEle('tbCart').style.display = "block";
     };
-    renderListCart(arrCart);
+    renderListCart(cart);
 }
 
 const renderListCart = (data) => {
@@ -115,15 +140,15 @@ const renderListCart = (data) => {
                     <img class="item_Img" src="${item.img}" style="width: 30%;" alt="">
                 </div>
                 <div class="col-4">
-                    <span class="col-4">${item.name}</span>
+                    <span class="col-4">${item.ten}</span>
                 </div>
                 <div class="col-2 icon">
                     <i class="fa-solid fa-chevron-left"></i>
-                    <span class="so_luong">1</span>
+                    <span class="so_luong">${item.soLuong}</span>
                     <i class="fa-solid fa-chevron-right"></i>
                 </div>
                 <div class="col-2">
-                    <span>${item.price}</span>
+                    <span>${item.gia}</span>
                 </div>
                 <div class="col-1">
                     <i class="fa-solid fa-trash"></i>
--- theme/src/models/listProduct.js
+++ theme/src/models/listProduct.js
@@ -1,7 +1,5 @@
 class ListProduct{
-    arr = [];
+    list = [];
 
-    addList(){
-        
-    }
+    
 }
